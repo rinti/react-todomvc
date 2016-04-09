40c7bc8ee1ef62e65f65eeb6ae5f79ce979d7cdc
rinti <andreas@agoodid.se>
2016-04-09 21:46:20 +0200

    more refactor, add a TodoItem-component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
 export default class TodoList extends React.Component {
   render() {
     return <section className="main">
           <ul className="todo-list">
             {this.props.todos.map(item =>
-              <li className="active" key={item.get('text')}>
-                <div className="view">
-                  <input type="checkbox" className="toggle" />
-                  <label htmlFor="todo">
-                    {item.get('text')}
-                  </label>
-                  <button className="destroy"></button>
-                </div>
-              </li>
+              <TodoItem text={item.get('text')} key={item.get('text')} />
             )}
           </ul>
         </section>
