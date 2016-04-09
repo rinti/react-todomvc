b452d234b29ba588eac76a160814e7c3b6382797
rinti <andreas@agoodid.se>
2016-04-09 19:56:03 +0200

    initial version

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class TodoApp extends React.Component {
+  getItems() {
+    return this.props.todos || [];
+  }
+  render() {
+    return <div>
+        <section className="todoApp">
+          <section className="main">
+            <ul className="todo-list">
+              {this.getItems().map(item =>
+                <li className="active" key={item.get('text')}>
+                  <div className="view">
+                    <input type="checkbox" className="toggle" />
+                    <label htmlFor="todo">
+                      {item.get('text')}
+                    </label>
+                    <button className="destroy"></button>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </section>
+        </section>
+      </div>
+  }
+};
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {List, Map} from 'immutable';
+
+import TodoApp from './components/TodoApp';
+
+const todos = List.of(
+  Map({id: 1, text: 'Cool', status: 'active', editing: false}),
+  Map({id: 2, text: 'Stuff', status: 'completed', editing: false}),
+  Map({id: 3, text: 'Hello', status: 'active', editing: false}),
+  Map({id: 4, text: 'Ses', status: 'active', editing: false})
+);
+
+ReactDOM.render(
+  <TodoApp todos={todos} />,
+  document.getElementById('app')
+);
